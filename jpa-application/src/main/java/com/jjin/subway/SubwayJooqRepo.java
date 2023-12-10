763c1a2595a3fd3a8c84e7389acc164cfa6fcd0a
jin <mydndb13@naver.com>
2023-12-10 19:02:27 +0900

    jpa jooq gradle 누락 파일

---
+++ jpa-application/src/main/java/com/jjin/subway/SubwayJooqRepo.java
@@ -0,0 +1,7 @@
+package com.jjin.subway;
+
+import org.springframework.stereotype.Repository;
+
+@Repository
+public class SubwayJooqRepo {
+}
---
+++ jpa-application/src/main/java/com/jjin/subway/SubwayService.java
@@ -0,0 +1,8 @@
+package com.jjin.subway;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class SubwayService {
+
+}
